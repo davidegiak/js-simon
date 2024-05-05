310df4dfefaf5b9b0f18951e0fbff2ff7b787d9e
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-05 11:50:39 +0200

    milestone 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Generate 5 numeri casuali. Può tornarvi utile una funzione? Provate a stamparli prima in console e poi in pagina.
 
 let numbers = [];
+const userNumbers = [];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
@@ -11,12 +12,19 @@ while (numbers.length < 5) {
     let numeriInPagina = document.querySelector("h1");
     if (!numbers.includes(numeroSingolo)) {
         numbers.push(numeroSingolo);
-
-        numeriInPagina.append(numeroSingolo + ",");
     }
     // Impostate un timer a 3 secondi, testatelo con un console.log. Una volta funzionante fate in modo che nasconda i numeri in pagina.
-    
     setTimeout (function(){
         console.log(numeroSingolo);
+        numeriInPagina.append(numeroSingolo + ",");
+        // Sempre nella funzione temporizzata, mostrate un prompt all'utente finchè non vi ritrovate con 5 numeri salvati. Poi stampate tutto l'array dei numeri utente in console.
+        while (userNumbers.length < 5) {
+            let newUsers = prompt("inserisci un numero");
+            userNumbers.push(newUsers);
+            console.log(userNumbers);
+        }
+
     }, 1000 * 3);
 }
+
+
