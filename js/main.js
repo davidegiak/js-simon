185008ef6dabc515dc1797c934b853e5335da572
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-05 13:23:04 +0200

    non completato ultimo milestone

--- js/main.js
+++ js/main.js
@@ -2,27 +2,41 @@
 
 let numbers = [];
 const userNumbers = [];
+let vincenti = [];
+let newUsers;
+let numeroSingolo;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
   }
 
 while (numbers.length < 5) {
-    let numeroSingolo = getRandomInt(100)
+    numeroSingolo = getRandomInt(100)
     let numeriInPagina = document.querySelector("h1");
     if (!numbers.includes(numeroSingolo)) {
         numbers.push(numeroSingolo);
+        console.log(numbers);
+        numeriInPagina.append(numeroSingolo + ",");
     }
     // Impostate un timer a 3 secondi, testatelo con un console.log. Una volta funzionante fate in modo che nasconda i numeri in pagina.
     setTimeout (function(){
-        console.log(numeroSingolo);
-        numeriInPagina.append(numeroSingolo + ",");
+        numeriInPagina.innerHTML = "";
+        numbers = 
         // Sempre nella funzione temporizzata, mostrate un prompt all'utente finchè non vi ritrovate con 5 numeri salvati. Poi stampate tutto l'array dei numeri utente in console.
         while (userNumbers.length < 5) {
-            let newUsers = prompt("inserisci un numero");
+            newUsers = prompt("inserisci un numero");
             userNumbers.push(newUsers);
             console.log(userNumbers);
+            if (numbers == newUsers) {
+                vincenti.push(newUsers);
+                console.log(newUsers);
+            }
         }
+        // else if (newUsers == numeroSingolo) {
+        //     vincenti.push(numerosingolo);
+        //     console.log("u loose"); 
+        //     console.log(vincenti);
+        // }
 
     }, 1000 * 3);
 }
